8f4b2755167645b6da37c3d2a653ce351e50a8cf
사현빈 <sahyeonbin@sahyeonbin-ui-MacBookAir.local>
2020-01-22 13:21:43 +0900

    REST, 등록, 수정, 조회, 삭제 완료 및 테스트

--- src/main/java/com/benworld/persistence/ReplyDAO.java
+++ src/main/java/com/benworld/persistence/ReplyDAO.java
@@ -2,6 +2,7 @@ package com.benworld.persistence;
 
 import java.util.List;
 
+import com.benworld.domain.Criteria;
 import com.benworld.domain.ReplyVO;
 
 public interface ReplyDAO {
@@ -10,4 +11,6 @@ public interface ReplyDAO {
 	//public void read(int rno)throws Exception;
 	public void update(ReplyVO vo)throws Exception;
 	public void delete(int rno)throws Exception;
+	public List<ReplyVO> listPage(Integer bno, Criteria cri) throws Exception;
+	public int count(Integer bno)throws Exception;
 }
